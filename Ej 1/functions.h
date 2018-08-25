6f9c0a744905cdfe2f6b0037dcd2bc5a68c9e786
sbualo <sbualo@itba.edu.ar>
2018-08-25 18:13:53 -0300

    Ejercicio 1 casi terminado
    pulir prolijidad + extras

---
+++ Ej 1/functions.h
@@ -0,0 +1,15 @@
+//
+//  functions.h
+//  Ej 1
+//
+//  Created by Santiago Bualo on 25/8/18.
+//  Copyright © 2018 Santiago Bualo. All rights reserved.
+//
+
+#ifndef functions_h
+#define functions_h
+
+#include <stdio.h>
+float exp_base_two (int n);
+//int validate_characters(int argc,const char**argv,int is_signed,int parte_entera,int parte_fraccionaria);
+#endif /* functions_h */
