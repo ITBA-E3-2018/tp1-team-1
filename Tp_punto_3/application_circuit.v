4cc7c0248e1e88680c9be02e5bcece579cd4cfd7
Gonzalo <gsilva@itba.edu.ar>
2018-08-25 21:07:50 -0300

    decoder

--- Tp_punto_3/application_circuit.v
+++ Tp_punto_3/application_circuit.v
@@ -2,21 +2,39 @@ module encoder (in, out);
 
     input[3:0] in;
     output reg[1:0] out;
-    reg error = 0;
         
     always @(in) begin
-        if (in == 'b0001)
+        if (in == 'b1000)
             out = 'b00;
         else
-            if (in == 'b0010)
+            if (in == 'b0100)
                 out = 'b01;
             else
-                if (in == 'b0100)
+                if (in == 'b0010)
                     out = 'b10;
                 else
-                    if (in == 'b1000)
+                    if (in == 'b0001)
                         out = 'b11;
-                    else  
-                        error = 1;
+    end
+endmodule
+
+
+module decoder (in, out);
+
+    input [1:0] in;
+    output reg[3:0] out;
+
+    always @(in) begin
+      if (in == 'b00)
+        out = 'b1000;
+    else 
+        if (in == 'b01)
+            out = 'b0100;
+        else
+            if (in == 'b10)
+                out = 'b0010;
+            else
+                if (in == 'b11)
+                    out = 'b0001;
     end
 endmodule
--- Tp_punto_3/main.v
+++ Tp_punto_3/main.v
@@ -1,16 +1,20 @@
 module circuit_test;
-reg[3:0] in;
+reg[3:0] in1;
+reg[1:0] in2;
 reg dummy;
-wire[1:0] out;
+wire[1:0] out1;
+wire[3:0] out2;
 
-encoder dut_module1 (in, out);
+encoder dut_module1 (in1, out1);
+decoder dut_module2 (in2, out2);
 
     initial begin
-        dummy = $value$plusargs("in=%b", in);
+        dummy = $value$plusargs("in1=%b", in1);
+        dummy = $value$plusargs("in2=%b", in2);
 
-        $display("input values are: %b", in);
+        $display("input values are: %b %b", in1, in2);
         #1;
-        $display("output values are: %b", out);
+        $display("output values are: %b %b", out1, out2);
         $finish;
     end
 endmodule
