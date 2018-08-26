f651e852f6063a77db3b455271b03896543bf76a
Gonzalo <gsilva@itba.edu.ar>
2018-08-26 00:04:44 -0300

    mux

--- Tp_punto_3/application_circuit.v
+++ Tp_punto_3/application_circuit.v
@@ -38,3 +38,25 @@ module decoder (in, out);
                     out = 'b0001;
     end
 endmodule
+
+
+module mux (in, out, select_lines);
+
+    input [3:0] in;
+    output reg out;
+    input [1:0] select_lines;
+
+    always @(in) begin
+        if (select_lines == 'b00)
+            out = in[3:3];
+        else   
+            if (select_lines == 'b01)
+                out = in[2:2];
+            else
+                if (select_lines == 'b10)
+                    out = in[1:1];
+                else   
+                    if (select_lines == 'b11)
+                        out = in[0:0];
+    end
+endmodule
--- Tp_punto_3/main.v
+++ Tp_punto_3/main.v
@@ -1,20 +1,28 @@
 module circuit_test;
 reg[3:0] in1;
 reg[1:0] in2;
+reg[3:0] in3;
+
 reg dummy;
 wire[1:0] out1;
 wire[3:0] out2;
+wire out3;
+
+reg [1:0] select_lines;
 
 encoder dut_module1 (in1, out1);
 decoder dut_module2 (in2, out2);
+mux dut_module3 (in3, out3, select_lines);
 
     initial begin
         dummy = $value$plusargs("in1=%b", in1);
         dummy = $value$plusargs("in2=%b", in2);
+        dummy = $value$plusargs("in3=%b", in3);
+        dummy = $value$plusargs("select_lines=%b", select_lines);
 
-        $display("input values are: %b %b", in1, in2);
+        $display("input values are: %b %b %b %b", in1, in2, in3, select_lines);
         #1;
-        $display("output values are: %b %b", out1, out2);
+        $display("output values are: %b %b %b", out1, out2, out3);
         $finish;
     end
 endmodule
